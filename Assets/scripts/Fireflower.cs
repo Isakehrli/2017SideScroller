489c4185b884607aca533daa8a5abe861c560d99
Unknown <kehrlii1@hawkmail.newpaltz.edu>
2017-05-14 00:44:39 -0400

    added other levels

---
+++ Assets/scripts/Fireflower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireflower : weapon
+{
+    public GameObject fireballPrefab;
+    public override void Attack()
+    {
+        var fireball = Instantiate(fireballPrefab);
+        fireball.transform.position = transform.position;
+        fireball.GetComponent<Rigidbody2D>().velocity = new Vector2(10, 0);
+        base.Attack();
+    }
+}
+	
